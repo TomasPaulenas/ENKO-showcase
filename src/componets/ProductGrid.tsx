fe1c51831d60a8514315f4377a59d8d37d59e578
TomasPaulenas <tomaspaulenas@hotmail.com>
2025-11-26 10:57:35 +0100

    responsive layout update

--- src/componets/ProductGrid.tsx
+++ src/componets/ProductGrid.tsx
@@ -2,28 +2,27 @@ import { products } from "../data/product";
 import { ProductCard } from "./ProductCard";
 
 export const ProductGrid = () => {
-    // asumimos que tenés 3 productos
+
     const [p1, p2, p3] = products;
 
     return (
-        <div className="grid grid-cols-1 lg:grid-cols-2 gap-10 place-items-center mt-6">
-            {/* Fila superior: 2 productos */}
-            <div className="w-full max-w-[380px]">
-                <ProductCard product={p1} />
-            </div>
+        <div className="mt-6 max-w-5xl mx-auto px-4 sm:px-6 lg:px-8">
+            <div className="grid grid-cols-1 md:grid-cols-2 gap-6 sm:gap-8 lg:gap-10">
 
-            <div className="w-full max-w-[380px]">
-                <ProductCard product={p2} />
-            </div>
+                <div className="w-full max-w-[380px] mx-auto">
+                    <ProductCard product={p1} />
+                </div>
 
-            {/* Fila inferior: 1 producto centrado */}
-            <div className="lg:col-span-2 flex justify-center w-full">
-                <div className="w-full max-w-[380px]">
-                    <ProductCard product={p3} />
+                <div className="w-full max-w-[380px] mx-auto">
+                    <ProductCard product={p2} />
+                </div>
+
+                <div className="md:col-span-2 flex justify-center w-full">
+                    <div className="w-full max-w-[380px] mx-auto">
+                        <ProductCard product={p3} />
+                    </div>
                 </div>
             </div>
         </div>
     );
 };
-
-
